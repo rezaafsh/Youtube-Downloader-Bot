9d1c658a845d5614b5698f71d1d114fd7651541e
Reza Afsh <rezaafsh76@gmail.com>
2021-06-06 11:30:06 +0430

    Changed Link

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/Dolatghavi_IranGhavi")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/Rezaaf76")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"سلام <b>{message.from_user.first_name}</b> را بزنید\n/help برای اطلاعات بیشتر "
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
